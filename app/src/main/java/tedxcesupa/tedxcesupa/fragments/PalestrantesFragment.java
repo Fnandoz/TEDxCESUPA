b59a140a556f2570d95916a91e739b9428fc0d22
Fernando Gomes <luis.sales@sead.pa.gov.br>
2018-03-26 15:13:05 -0300

    Retirado exibir pontuacao dos palestrantes

--- app/src/main/java/tedxcesupa/tedxcesupa/fragments/PalestrantesFragment.java
+++ app/src/main/java/tedxcesupa/tedxcesupa/fragments/PalestrantesFragment.java
@@ -218,7 +218,7 @@ public class PalestrantesFragment extends Fragment {
                         soma_avaliacao += Float.parseFloat(i.toString());
                     }
                     media_avaliacao = soma_avaliacao / avaliacoes_palestrante_map.size();
-                    ratingPalestrante.setRating(media_avaliacao);
+                    //ratingPalestrante.setRating(media_avaliacao);
                 }
             }
 
